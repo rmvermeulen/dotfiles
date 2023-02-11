9e9f0b542b5ce6bc8ef432efa59c9541c931d978
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2023-02-11 19:38:41 +0100

    fix issue with managers depending on each other

--- src/scripts/setup-node-etc.sh
+++ src/scripts/setup-node-etc.sh
@@ -1,23 +1,43 @@
 # node stuff
 
-# setup swpm, if we use it
-if type swpm &>/dev/null; then
-    source $DOTFILES_SCRIPTSDIR/node/setup-swpm.sh
-fi
+did_swpm=false
+did_yarn=false
+did_pnpm=false
+did_bun=false
+# need to repeat since one of these might be installing the others
+for i in {0..1}
+do
+    # setup swpm, if we use it
+    if type swpm &>/dev/null; then
+        if [ "$did_swpm" != true ]; then
+            did_swpm=true
+            source $DOTFILES_SCRIPTSDIR/node/setup-swpm.sh
+        fi
+    fi
 
-# setup yarn, if we use it
-if type yarn &>/dev/null; then
-    source $DOTFILES_SCRIPTSDIR/node/setup-yarn.sh
-fi
+    # setup yarn, if we use it
+    if type yarn &>/dev/null; then
+        if [ "$did_yarn" != true ]; then
+            did_yarn=true
+            source $DOTFILES_SCRIPTSDIR/node/setup-yarn.sh
+        fi
+    fi
 
-# setup pnpm, if we use it
-if type pnpm &>/dev/null; then
-    source $DOTFILES_SCRIPTSDIR/node/setup-pnpm.sh
-fi
+    # setup pnpm, if we use it
+    if type pnpm &>/dev/null; then
+        if [ "$did_pnpm" != true ]; then
+            did_pnpm=true
+            source $DOTFILES_SCRIPTSDIR/node/setup-pnpm.sh
+        fi
+    fi
 
-# setup bun.sh, if we use it
-if type bun &>/dev/null; then
-    source $DOTFILES_SCRIPTSDIR/node/setup-bun.sh
-fi
+    # setup bun.sh, if we use it
+    if type bun &>/dev/null; then
+        if [ "$did_bun" != true ]; then
+            did_bun=true
+            source $DOTFILES_SCRIPTSDIR/node/setup-bun.sh
+        fi
+    fi
+done
 
 echo "node package manager(s) ✔"
